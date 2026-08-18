3af76eec7f4257f442e8da2e76055a8b322c6d6f
3bood-dev <abdallah.abdelwahab377@gmail.com>
2026-08-19 00:20:47 +0300

    Implement deposit function and balance mapping in Vault contract

--- src/Vault.sol
+++ src/Vault.sol
@@ -2,5 +2,10 @@
 pragma solidity ^0.8.31;
 
 contract Vault {
-    
+     mapping(address => uint256) private balances;
+
+    function deposit() external payable {
+        require(msg.value > 0, "Send some ETH");
+        balances[msg.sender] += msg.value;
+    }
 }
